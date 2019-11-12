ddefdb680ea1b649137aaf07b22c45511d06f134
Br0Fi <l.segger@web.de>
2019-11-12 18:32:48 +0100

    A2 basically

--- Zettel3/A2.py
+++ Zettel3/A2.py
@@ -15,12 +15,10 @@ matplotlib.rcParams.update({'font.size': fig_labelsize})
 colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
 
 #Aufgabenspezifische Konstanten:
-ks = 0.799
-ds = 9.44
-ns = 14.68
-fs = 2.1
-omega = 2.25
-part = "d"
+sigma = 10
+bb = 8/3
+rr = 0.5
+part = "a"
 
 # Hauptprogramm:
 
@@ -29,50 +27,61 @@ t0 = 0
 T = 200
 h = 0.005
 # Anfangswerte
-x0 = -1.5*math.pi/2
-y0 = 0
-low_cut = 10000
-xy_cutoff = 8000 + low_cut
+x0 = 50
+y0 = 50
+z0 = 50
+low_cut = 0
+xy_cutoff = 0 + low_cut
 
 # verwende "ein klassisches RK4":
-def rk4_2d(t0,x0,y0,T,h):
+def rk4_2d(t0,x0,y0,z0,T,h):
     N = int(T/h)
     t = np.linspace(t0, T, N+1)
     x = np.empty(N+1)
     y = np.empty(N+1)
+    z = np.empty(N+1)
     x[0] = x0
     y[0] = y0
-    def fx(x_arg, y_arg, t_arg):
-        return y_arg
-    def fy(x_arg, y_arg, t_arg):
-        return -ds*x_arg + fs*math.sin(omega*t_arg) \
-               - ks*y_arg + ns*math.sin(x_arg)
+    z[0] = y0
+    def fx(x_arg, y_arg, z_arg, t_arg):
+        return sigma * (y_arg - x_arg)
+    def fy(x_arg, y_arg, z_arg, t_arg):
+        return rr*x_arg - y_arg - x_arg * z_arg
+    def fz(x_arg, y_arg, t_arg):
+        return x_arg * y_arg - bb * z_arg
 
     for i in range(N):
-        kx1 = fx(x[i],             y[i]            , t[i] +h/2)
-        ky1 = fy(x[i],             y[i]            , t[i] +h/2)
-        kx2 = fx(x[i] + h/2 * kx1, y[i] + h/2 * ky1, t[i] +h/2)
-        ky2 = fy(x[i] + h/2 * kx1, y[i] + h/2 * ky1, t[i] +h/2)
-        kx3 = fx(x[i] + h/2 * kx2, y[i] + h/2 * ky2, t[i] +h/2)
-        ky3 = fy(x[i] + h/2 * kx2, y[i] + h/2 * ky2, t[i] +h/2)
-        kx4 = fx(x[i] + h   * kx3, y[i] + h   * ky3, t[i] +h/2)
-        ky4 = fy(x[i] + h   * kx3, y[i] + h   * ky3, t[i] +h/2)
+        kx1 = fx(x[i],             y[i]            , z[i], t[i] +h/2)
+        ky1 = fy(x[i],             y[i]            , z[i], t[i] +h/2)
+        kz1 = fz(x[i],             y[i]            , z[i], t[i] +h/2)
+        kx2 = fx(x[i] + h/2 * kx1, y[i] + h/2 * ky1, z[i]+h/2*kz1, t[i] +h/2)
+        ky2 = fy(x[i] + h/2 * kx1, y[i] + h/2 * ky1, z[i]+h/2*kz1, t[i] +h/2)
+        kz2 = fz(x[i] + h/2 * kx1, y[i] + h/2 * ky1, z[i]+h/2*kz1, t[i] +h/2)
+        kx3 = fx(x[i] + h/2 * kx2, y[i] + h/2 * ky2, z[i]+h/2*kz2, t[i] +h/2)
+        ky3 = fy(x[i] + h/2 * kx2, y[i] + h/2 * ky2, z[i]+h/2*kz2, t[i] +h/2)
+        kz3 = fz(x[i] + h/2 * kx2, y[i] + h/2 * ky2, z[i]+h/2*kz2, t[i] +h/2)
+        kx4 = fx(x[i] + h   * kx3, y[i] + h   * ky3, z[i]+h*kz3, t[i] +h/2)
+        ky4 = fy(x[i] + h   * kx3, y[i] + h   * ky3, z[i]+h*kz3, t[i] +h/2)
+        kz4 = fz(x[i] + h   * kx3, y[i] + h   * ky3, z[i]+h*kz3, t[i] +h/2)
 
         x[i+1] = x[i] + h/6 * (kx1 + 2*kx2 + 2*kx3 + kx4)
         y[i+1] = y[i] + h/6 * (ky1 + 2*ky2 + 2*ky3 + ky4)
-    return t,x,y
+        z[i+1] = z[i] + h/6 * (kz1 + 2*kz2 + 2*kz3 + kz4)
+    return t,x,y,z
 
 
 fig, ax1 = plt.subplots(figsize=fig_size)
-plt.title("Pohlsches Rad, x0 = " + str(round(x0, 5)) +
-        ", y0 = " + str(y0) + ", h = " + str(h) +", Omega= " + str(omega))
+plt.title("Lorenz-System, x0 = " + str(round(x0, 5)) +", y0 = " + str(y0) +
+        ", z0 = " + str(z0) + ", h = " + str(h) +", r= " + str(rr))
 
-t, x, y = rk4_2d(t0, x0, y0, T, h)
+t, x, y, z = rk4_2d(t0, x0, y0, z0, T, h)
 
 color = "tab:red"
+plt.legend(prop={'size':fig_legendsize})
 ax1.set_xlabel('t')
-ax1.set_ylabel('x (grad)', color=color)
+ax1.set_ylabel('x, z', color=color)
 ax1.plot(t[low_cut:xy_cutoff], x[low_cut:xy_cutoff], label="x(t)", color=color)
+ax1.plot(t[low_cut:xy_cutoff], x[low_cut:xy_cutoff], label="z(t)", color="tab:green")
 plt.grid()
 ax1.tick_params(axis='y', labelsize=fig_labelsize, labelcolor=color)
 ax2 = ax1.twinx() # instantiate a second axes that shares the same x-axis
@@ -81,7 +90,7 @@ ax2.set_ylabel('y', color=color)
 ax2.plot(t[low_cut:xy_cutoff],y[low_cut:xy_cutoff], label="y(t)", color=color)
 ax2.tick_params(axis='y', labelsize=fig_labelsize, labelcolor=color)
 fig.tight_layout()
-plt.savefig("Zettel3/figures/A1-" + part + ".png")
+#plt.savefig("Zettel3/figures/A2-" + part + ".png")
 plt.show()
 
 # %%
@@ -95,27 +104,27 @@ def return_map(t,x):
     return tmax, xmax
 
 fig = plt.figure(figsize=fig_size)
-tmax, xmax = return_map(t,x)
-plt.plot(tmax, xmax,".")
+tmax, zmax = return_map(t,x)
+plt.plot(tmax, zmax,".")
 plt.grid()
-plt.ylabel('x_max')
+plt.ylabel('z_max')
 plt.xlabel("t_max")
 plt.tick_params(labelsize=fig_labelsize)
 plt.show()
 
 fig = plt.figure(figsize=fig_size)
 plt.title("Return Plot")
-plt.xlim([2,2.4])
-plt.ylim([2,2.4])
-xmax = xmax[int(len(xmax)/2):-1] # crop tune in time and crop last maximum (not actual maximum)
-phi_n = xmax[:-1]
-phi_n1 = xmax[1:]
+#plt.xlim([2,2.4])
+#plt.ylim([2,2.4])
+zmax = zmax[int(len(zmax)/2):-1] # crop tune in time and crop last maximum (not actual maximum)
+phi_n = zmax[:-1]
+phi_n1 = zmax[1:]
 plt.plot(phi_n, phi_n1,".")
 
 plt.grid()
-plt.ylabel('phi_(n+1)')
-plt.xlabel("phi_n")
+plt.ylabel('z_max(n+1)')
+plt.xlabel("Z_max(n)")
 plt.tick_params(labelsize=fig_labelsize)
-plt.savefig("Zettel3/figures/A1-" + part + "-return.png")
+#plt.savefig("Zettel3/figures/A2-" + part + "-return.png")
 plt.show()
 # %%
