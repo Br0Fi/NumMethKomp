f2b5bdca67c79f02ca10976c9081d82e263b36c0
Br0Fi <l.segger@web.de>
2020-05-26 14:36:13 +0200

    Task 1 works; gif provided

--- SS/Zettel4/task1.py
+++ SS/Zettel4/task1.py
@@ -12,15 +12,11 @@ import matplotlib.pyplot as plt
 
 L = 2.0 * np.pi     #Interval length
 N = 256           #Number of points on [-L/2, L/2], N = 2^p
-Tend= 20000          # t\in[0,Tend]
-h=0.000001              # Time step
+Tend= 40000          # t\in[0,Tend]
+h=0.00001              # Time step
 c1=5
 c2=4
 
-"""x = (np.arange(N)-N/2) * L/N #x-Array		# Interval [-pi,pi]
-k = np.zeros(N) #Definition of the wavenumbers		# Alternative definition of k-grid
-k[0:N/2+1] = np.arange(N/2+1)*2*np.pi/L
-k[N/2+1:] = np.arange(-N/2+1,0)*2*np.pi/L"""
 x = np.arange(N) * L/N - L/2 #x-Array from -pi to pi
 dx=L/N
 k = np.fft.fftfreq(N,dx/(2.*np.pi))
@@ -30,18 +26,16 @@ u0= c1**2/2/np.cosh(c1*(x+2)/2)**2 + c2**2/2/np.cosh(c2*(x+1)/2)**2
 uk = np.fft.fft(u0) # uk=fft(u0)
 wk = uk
 
-def dealiase(u): #how is that spelled?
-	return u
-
 def rhs(k,wk,tt): # RHS in the Fourier space
-	expk3 = np.exp(-1j*k**3*tt)
-	return -3*1j*k* expk3* np.fft.fft(np.fft.ifft(expk3*wk)**2)
+	expmk3 = np.exp(-1j*k**3*tt)
+	exppk3 = np.exp(1j*k**3*tt)
+	return -3*1j*k* expmk3* np.fft.fft(np.fft.ifft(exppk3*wk)**2)
 
 def rk4f(k,uk,tt):
     k1 =rhs(k,uk,tt)
-    k2 =rhs(k,uk+0.5*h*k1,tt)
-    k3 =rhs(k,uk+0.5*h*k2,tt)
-    k4 =rhs(k,uk+h*k3,tt)
+    k2 =rhs(k,uk+0.5*h*k1,tt+0.5*h)
+    k3 =rhs(k,uk+0.5*h*k2,tt+0.5*h)
+    k4 =rhs(k,uk+h*k3,tt+h)
     return uk + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
 
 ii=0
@@ -50,14 +44,14 @@ plt.ion()  # Force interactive; otherwise each timestep the figure has to be clo
 
 for n in range(Tend+1): # RK4 time loop in Fourier space
 
-    tt = ii*h
+    tt = n*h
     wk = rk4f(k,wk,tt)
 
-    if n % 50 == 0: # plot the solution in x-space
+    if n % 200 == 0: # plot the solution in x-space
 
         u = np.fft.ifft(np.exp(1j * k**3 * tt)*wk)
         plt.cla() #clear axis
-        #plt.ylim(-1,1)
+        plt.ylim(-0.5,17)
         plt.plot(x,u.real,'r.') # plot the numerical solution
         plt.ylabel("u(x,t)")
         plt.xlabel("x")
@@ -65,7 +59,7 @@ for n in range(Tend+1): # RK4 time loop in Fourier space
         plt.show()
         ii=ii+1
         filename = "pic{:03d}.png".format(ii)
-        #plt.savefig("pics/"+filename)
+        plt.savefig("pics/"+filename)
         plt.pause(0.01)# pause between frames
 
 
